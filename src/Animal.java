2df0078444b1f5c337dd9f6b84f3f85d08c20062
ShiryevIgorVl <ishirayev@gmail.com>
2020-08-04 13:14:27 +0300

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+    private String name;
+    private int runDog;
+    private int runCat;
+    private int sailDog;
+    private int sailCat;
+
+    public Animal(String name, int runDog, int runCat, int sailDog, int sailCat) {
+        this.name = name;
+        this.runDog = runDog;
+        this.runCat = runCat;
+        this.sailCat = sailCat;
+        this.sailDog = sailDog;
+    }
+
+    
+}
